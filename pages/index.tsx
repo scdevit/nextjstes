210d89a2d7069d44bf7d4fee8d4a3108ae15f064
okki23 <okkisetyawan@gmail.com>
2022-12-12 18:26:53 +0700

    updatelagi

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Testing dev to <a href="https://nextjs.org">Next.js!</a>
+          Halo Okki dev to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
